903ee714f518fda4e554e2b1a7fe7a61ea0aa654
BigN03 <channelbign@gmail.com>
2024-08-10 20:34:17 +0700

    Update compAppHeader.tsx

--- src/components/compAppHeader.tsx
+++ src/components/compAppHeader.tsx
@@ -13,6 +13,15 @@ export default function CompAppHeader() {
         height={80}
         priority
       />
+      <Script async src="https://www.googletagmanager.com/gtag/js?id=AW-16597066183" />
+
+      <Script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag()
+      	{dataLayer.push(arguments);}
+        	gtag('js', new Date());
+        	gtag('config', 'AW-16597066183');
+        />
     </div>
   </header>
 }
