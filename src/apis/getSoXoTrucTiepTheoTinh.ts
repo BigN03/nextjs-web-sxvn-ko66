90db31640f9983ae68a1c6689a2b3384859b237e
BigN03 <channelbign@gmail.com>
2024-08-10 14:11:24 +0700

    Update getSoXoTrucTiepTheoTinh.ts

--- src/apis/getSoXoTrucTiepTheoTinh.ts
+++ src/apis/getSoXoTrucTiepTheoTinh.ts
@@ -1,5 +1,5 @@
 const getSoXoTrucTiepTheoTinh = async (mien: string, locate: string, time: string) => {
-  const staticData = await fetch(`http://api.sa88.art/api/web-sxvn/crawls/get-so-xo-theo-tinh?locate=${mien}&province=${locate}&date=${time}`);
+  const staticData = await fetch(`https://api.sa88.art/api/web-sxvn/crawls/get-so-xo-theo-tinh?locate=${mien}&province=${locate}&date=${time}`);
   const res = await staticData.json()
   if (res.status == 200) {
     return res.data
