59bb43c24b68e1c8a5fe45ad7a89bfe4fa2571a6
BigN03 <channelbign@gmail.com>
2024-08-10 16:02:40 +0700

    Update compAppHeader.tsx

--- src/components/compAppHeader.tsx
+++ src/components/compAppHeader.tsx
@@ -3,17 +3,6 @@ import Image from "next/image";
 import 'moment/locale/vi'
 export default function CompAppHeader() {
   return <header className="container mx-auto py-2">
-   
-    {/* <!-- Google tag (gtag.js) --> */}
-      <script async src="https://www.googletagmanager.com/gtag/js?id=AW-16597066183">
-      </script>
-      <script>
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-
-        gtag('config', 'AW-16597066183');
-      </script>
     <div className="flex flex-row justify-center items-center">
       <Image
         className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
@@ -23,6 +12,6 @@ export default function CompAppHeader() {
         height={80}
         priority
       />
-       </div>
+    </div>
   </header>
 }
