c63f475e87d7689d7a207eab2a361b9470c8f47e
BigN03 <channelbign@gmail.com>
2024-08-10 21:03:35 +0700

    Update compAppHeader.tsx

--- src/components/compAppHeader.tsx
+++ src/components/compAppHeader.tsx
@@ -1,11 +1,12 @@
 import moment from "moment";
 import Image from "next/image";
-import 'moment/locale/vi'
 import { Html, Head, Main, NextScript } from 'next/document'
 import Script from 'next/script'
+import 'moment/locale/vi'
 
 export default function CompAppHeader() {
-  return <header className="container mx-auto py-2">
+  return <Header className="container mx-auto py-2">
+
     <div className="flex flex-row justify-center items-center">
       <Image
         className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
@@ -16,4 +17,5 @@ export default function CompAppHeader() {
         priority
       />
     </div>
+  />
 }
