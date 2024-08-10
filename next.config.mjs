a41385b70253c818459315447e3587e02114316f
BigN03 <channelbign@gmail.com>
2024-08-10 15:50:22 +0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,6 @@ const nextConfig = {
         // Warning: This allows production builds to successfully complete even if
         // your project has ESLint errors.
         ignoreDuringBuilds: true,
-        nextScriptWorkers: true,
       },
 };
 
